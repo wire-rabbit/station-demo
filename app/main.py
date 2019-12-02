8fd395584a483c0a821b0f791c7660e261d25831
Jeremy Bensman <jeremy@wirerabbit.com>
2019-12-02 08:06:58 -0800

    Initial Flask setup with SPA routes

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__,
+            static_folder='./static',
+            template_folder='./templates')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def global_route(path):
+    return "Hello SPA"
